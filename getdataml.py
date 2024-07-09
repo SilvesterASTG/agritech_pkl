52d3d7f753ee4997ed8ad2e76ce04dbe4603d8f0
SilvesterASTG <165285903+SilvesterASTG@users.noreply.github.com>
2024-07-09 13:28:07 +0700

    Update getdataml.py

--- getdataml.py
+++ getdataml.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta, date
 # Konfigurasi database
 db_config = {
     'user': 'root',
-    'password': '',
+    'password': 'agritech',
     'host': 'localhost',
     'database': 'agritech'
 }
